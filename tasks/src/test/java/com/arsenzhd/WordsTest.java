f5415dc6d0636f13a793f367731906b5c3ea10bf
Arsenii <arsen2@ukr.net>
2017-04-08 19:51:37 +0300

    Rename WordsUtil to WordsServices create findDistinctWordsInFile method
    Create method for word task and test for it

--- tasks/src/test/java/com/arsenzhd/WordsTest.java
+++ tasks/src/test/java/com/arsenzhd/WordsTest.java
@@ -1,7 +1,34 @@
 package com.arsenzhd;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.arsenzhd.services.WordsServices.findDistinctWordsInFile;
+
 /**
  * Created by ArsZhd on 08.04.17.
  */
 public class WordsTest {
+    private static Set<String> words;
+    @BeforeClass
+    public static void init() {
+        words = new HashSet<>();
+        words.add("Word");
+        words.add("word");
+        words.add("No");
+        words.add("test");
+    }
+
+    @Test
+    public void testFindDistinctWordsInFile() throws IOException {
+        Assert.assertEquals(words,
+                findDistinctWordsInFile(System.getProperty("user.dir")
+                        +
+                        "/src/main/resources/test.txt", ".*[.:,/;].*"));
+    }
 }
